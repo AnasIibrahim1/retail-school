48ac1e948e26b651de3fc6463daf95dc82e70a64
Anas Ibrahim <anas.ibrahim@the-doable.com>
2025-09-23 15:42:33 +0300

    Updated

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,11 @@
 import CourseCard from "@/components/cards/CourseCard/CourseCard";
-import Header from "@/components/Header/Header";
-import Footer from "@/components/Footer/Footer";
+import HomeBackCircle from "@/components/backCircles/HomeBackCircle/HomeBackCircle";
 
 export default function Home() {
   return (
     <>
     <CourseCard title="Course 1" image="https://via.placeholder.com/150" price={100} />
+    <HomeBackCircle />
     </>
   );
 }
--- src/components/backCircles/HomeBackCircle/HomeBackCircle.jsx
+++ src/components/backCircles/HomeBackCircle/HomeBackCircle.jsx
@@ -1,7 +1,10 @@
 export default function HomeBackCircle() {
   return (
     <div>
-      <div></div>
+      <div style={{poistion:"relative"}}>
+        <img src="/shapes/landing-circle.svg" alt="Home Back Circle" />
+        <img src="" alt="" />
+      </div>
     </div>
   )
 }
